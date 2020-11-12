f6b00944521582c5f533b451abb1b2a6a109d645
baxiang <919784497@qq.com>
2020-11-13 00:01:10 +0800

    freeswitch

---
+++ freeswitch/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'freeswitch' ]; then
+    chown -R freeswitch:freeswitch /etc/freeswitch
+    chown -R freeswitch:freeswitch /var/{run,lib}/freeswitch
+    chown -R freeswitch:freeswitch /{db,certs,scripts,recordings,tmp}
+
+	if [ -d /docker-entrypoint.d ]; then
+		for f in /docker-entrypoint.d/*.sh; do
+			[ -f "$f" ] && . "$f"
+		done
+	fi
+
+	exec freeswitch freeswitch -u freeswitch -g freeswitch -c \
+		-sounds /sounds -recordings /recordings -conf /etc/freeswitch \
+		-certs /certs -db /db -scripts /scripts	-log /tmp
+fi
+
+exec "$@"
